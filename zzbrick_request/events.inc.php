eff246ad59d59eebcea36a6fafbb77761d3823e9
Gustaf Mossakowski <gustaf@koenige.org>
2022-04-27 10:19:23 +0000

    events: set status code explicitly to 200 if events are found (if used in combination with other request scripts, e. g. news, that return 404)
    git-svn-id: file:///Users/Shared/Repos/modules/events/trunk@137 96fb0840-0139-4485-b0a6-37cbb47da54f

--- zzbrick_request/events.inc.php
+++ zzbrick_request/events.inc.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/events
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2014-2018, 2020-2021 Gustaf Mossakowski
+ * @copyright Copyright © 2014-2018, 2020-2022 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -95,6 +95,7 @@ function mod_events_events($params, $settings) {
 
 	if (!$events) {
 		$events['no_events'] = true;
+		$page['status'] = 404;
 	} else {
 		foreach ($events as $event_id => $event) {
 			if (empty($event['images'])) continue;
@@ -102,6 +103,7 @@ function mod_events_events($params, $settings) {
 				$page['extra']['magnific_popup'] = true;
 			break;
 		}
+		$page['status'] = 200;
 	}
 
 	$events['cal_title'] = '';
