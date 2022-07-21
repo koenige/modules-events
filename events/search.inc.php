ffe7a488ef050d0f62df75b9c34eb8f64f04af5b
Gustaf Mossakowski <gustaf@koenige.org>
2022-07-21 17:40:18 +0000

    search: add public event search
    git-svn-id: file:///Users/Shared/Repos/modules/events/trunk@152 96fb0840-0139-4485-b0a6-37cbb47da54f

---
+++ events/search.inc.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * events module
+ * search functions
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/events
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2020, 2022 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+function mf_events_search($q) {
+	$where_sql = '(event LIKE "%%%s%%"
+		OR abstract LIKE "%%%s%%"
+		OR events.description LIKE "%%%s%%"
+		OR category LIKE "%%%s%%")';
+	$where = [];
+	foreach ($q as $string) {
+		$where[] = sprintf($where_sql, $string, $string, $string, $string);
+	}
+	$sql = 'SELECT events.event_id
+			, CONCAT(date_begin, IFNULL(CONCAT("/", date_end), "")) AS duration
+			, event, abstract, identifier
+			, IF (takes_place = "yes", 1, NULL) AS takes_place
+			, category
+		FROM events
+		LEFT JOIN events_categories
+			ON events_categories.event_id = events.event_id
+			AND events_categories.type_category_id = %d
+		LEFT JOIN categories USING (category_id)
+		WHERE %s
+		AND published = "yes"
+		AND (ISNULL(categories.parameters) OR categories.parameters NOT LIKE "%%search=0%%")
+		ORDER BY IFNULL(date_begin, date_end) DESC, time_begin DESC, event';
+	$sql = sprintf($sql
+		, wrap_category_id('events')
+		, implode(' AND ', $where)
+	);
+	$data['events'] = wrap_db_fetch($sql, 'event_id');
+	$data['events'] = mf_events_media($data['events']);
+	return $data;
+}
+
+function mf_events_media($events) {
+	if (!$events) return [];
+	$media = wrap_get_media(array_keys($events), 'events', 'event');
+	foreach ($media as $id => $files) {
+		$events[$id] += $files;
+	}
+	return $events;
+}
