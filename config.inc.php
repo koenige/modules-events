0d756ec2146de1caf94235783efe2c19dda11dcd
Gustaf Mossakowski <gustaf@koenige.org>
2023-06-27 12:25:49 +0000

    use setting `zzform_nice_tablename`, $zz['footer']instead of deprecated $zz_conf-variable
    git-svn-id: file:///Users/Shared/Repos/modules/events/trunk@185 96fb0840-0139-4485-b0a6-37cbb47da54f

--- config.inc.php
+++ config.inc.php
@@ -8,11 +8,11 @@
  * https://www.zugzwang.org/modules/events
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2022 Gustaf Mossakowski
+ * @copyright Copyright © 2022-2023 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
-$zz_conf['nice_tablename']['events_categories'] = 'Categories of Events';
-$zz_conf['nice_tablename']['events_contacts'] = 'Contacts of an Event';
-$zz_conf['nice_tablename']['events_media'] = 'Media of an Event';
+wrap_setting('zzform_nice_tablename[events_categories]', 'Categories of Events');
+wrap_setting('zzform_nice_tablename[events_contacts]', 'Contacts of an Event');
+wrap_setting('zzform_nice_tablename[events_media]', 'Media of an Event');
