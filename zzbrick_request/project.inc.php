b70f3722beaf42bd711a91d60eae4000fd7667fd
Gustaf Mossakowski <gustaf@koenige.org>
2023-12-15 21:37:30 +0000

    project: return data in $page['data']
    git-svn-id: file:///Users/Shared/Repos/modules/events/trunk@260 96fb0840-0139-4485-b0a6-37cbb47da54f

--- zzbrick_request/project.inc.php
+++ zzbrick_request/project.inc.php
@@ -119,7 +119,8 @@ function mod_events_project($params) {
 		$page['extra']['class'] = 'unpublished';
 	if (!empty($event['menu_hierarchy']))
 		$page['extra']['menu_hierarchy'] = wrap_menu_hierarchy('events_projects', $event['menu_hierarchy'], 'category');
-	
+	$page['data'] = $event;
+
 	return $page;
 }
 
