35bb2f897624fb6ebffecba818b5b897d260ebd9
Gustaf Mossakowski <gustaf@koenige.org>
2024-04-08 10:00:28 +0000

    eventinternal: add basic internal event script, with submenu
    git-svn-id: file:///Users/Shared/Repos/modules/events/trunk@280 96fb0840-0139-4485-b0a6-37cbb47da54f

---
+++ zzbrick_request/eventinternal.inc.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * events module
+ * internal page for an event
+ *
+ * Part of »Zugzwang Project«
+ * https://www.zugzwang.org/modules/events
+ *
+ * @author Gustaf Mossakowski <gustaf@koenige.org>
+ * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
+ */
+
+
+function mod_events_eventinternal($params, $settings, $event) {
+	if (count($params) !== 2) return false;
+	if (!wrap_access('events_event_edit', $event['event_rights'])) wrap_quit(403);
+	
+	$page['text'] = wrap_template('eventinternal', $event);
+	return $page;
+}
