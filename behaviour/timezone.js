668a173d488dee55441bfcf0668337e47a1bdb69
Gustaf Mossakowski <gustaf@koenige.org>
2024-07-19 07:22:51 +0000

    timezone.js: remove dependency from moments.js, use Intl.DateTimeFormat instead
    git-svn-id: file:///Users/Shared/Repos/modules/events/trunk@297 96fb0840-0139-4485-b0a6-37cbb47da54f

--- behaviour/timezone.js
+++ behaviour/timezone.js
@@ -5,7 +5,7 @@
  * Part of »Zugzwang Project«
  * https://www.zugzwang.org/modules/events
  *
- * @copyright Copyright © 2020 Gustaf Mossakowski
+ * @copyright Copyright © 2020, 2024 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
  
@@ -24,24 +24,30 @@ function calculateNewTime() {
 	var startDate = '';
 	var endDate = '';
 
-	for (i = 0; i < times.length; i++) {
-		var m = moment(times[i].getAttribute('datetime') + timezone);
-		m.locale('en-gb');
-		if (times[i].getAttribute('itemprop') === 'startDate') {
-			startDate = m.format('ddd D MMM YYYY, HH.mm');
-		}
-		else if (times[i].getAttribute('itemprop') === 'endDate') {
-			endDate = m.format('ddd D MMM YYYY, HH.mm');
-			if (endDate.substring(0, 14) === startDate.substring(0, 14))
-				endDate = endDate.substring(17);
-		}
-	}
+    for (i = 0; i < times.length; i++) {
+        var date = new Date(times[i].getAttribute('datetime') + timezone);
+        var options = {
+        	weekday: 'short', day: 'numeric', month: 'short', year: 'numeric',
+        	hour: '2-digit', minute: '2-digit', hour12: false
+        };
+        var formattedDate = new Intl.DateTimeFormat('en-GB', options).format(date).replace(',', '').replace(':', '.');
+
+        if (times[i].getAttribute('itemprop') === 'startDate') {
+            startDate = formattedDate;
+        } else if (times[i].getAttribute('itemprop') === 'endDate') {
+            endDate = formattedDate;
+            if (endDate.substring(0, 14) === startDate.substring(0, 14))
+                endDate = endDate.substring(17);
+        }
+    }
+
 	var span = document.createElement('span');
 	if (endDate) {
 		var localTime = document.createTextNode(' (Your Time: ' +  startDate + '–' + endDate  + ' h)');
 	} else {
 		var localTime = document.createTextNode(' (Your Time: ' + startDate + ' h)');
 	}
+	span.appendChild(document.createElement('br'));
 	span.appendChild(localTime);
 	
 	timezones[0].appendChild(span);
