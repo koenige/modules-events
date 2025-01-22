f7335a74b06488f675923560aaff9f8fc4944431
Gustaf Mossakowski <gustaf@koenige.org>
2025-01-22 16:27:39 +0000

    contact: add translations; show projects in separate list below events
    git-svn-id: file:///Users/Shared/Repos/modules/events/trunk@328 96fb0840-0139-4485-b0a6-37cbb47da54f

--- events/contact.inc.php
+++ events/contact.inc.php
@@ -8,15 +8,16 @@
  * https://www.zugzwang.org/modules/events
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2024 Gustaf Mossakowski
+ * @copyright Copyright © 2024-2025 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
 
 function mf_events_contact($data, $ids) {
-	$sql = 'SELECT contact_id, event_id, event, identifier
+	$sql = 'SELECT event_contact_id, contact_id, event_id, event, identifier
 			, CONCAT(date_begin, IFNULL(CONCAT("/", date_end), "")) AS duration
-			, category as role
+			, category_id, category as role
+			, event_category_id
 	    FROM events_contacts
 	    LEFT JOIN events USING (event_id)
 	    LEFT JOIN categories
@@ -24,10 +25,21 @@ function mf_events_contact($data, $ids) {
 	    WHERE contact_id IN (%s)
 	    ORDER BY IFNULL(date_begin, date_end) DESC, events_contacts.sequence';
 	$sql = sprintf($sql, implode(',', $ids));
-	$events_per_contact = wrap_db_fetch($sql, ['contact_id', 'event_id']);
-	
-	foreach ($events_per_contact as $contact_id => $events)
-		$data[$contact_id]['events'] = $events;
+	$events = wrap_db_fetch($sql, 'event_id');
+	$events = wrap_translate($events, 'events', 'event_id');
+	$events = wrap_translate($events, 'contacts', 'contact_id');
+	$events = wrap_translate($events, ['role' => 'categories.category'], 'category_id');
+
+	foreach ($events as $event_contact_id => $event) {
+		if (!array_key_exists('events', $data[$event['contact_id']])) {
+			$data[$event['contact_id']]['events'] = [];
+			$data[$event['contact_id']]['projects'] = [];
+		}
+		if ($event['event_category_id'] === wrap_category_id('event/project'))
+			$data[$event['contact_id']]['projects'][$event['event_id']] = $event;
+		else
+			$data[$event['contact_id']]['events'][$event['event_id']] = $event;
+	}
 	
 	$data['templates']['contact_5'][] = 'contact-events';
 	return $data;
