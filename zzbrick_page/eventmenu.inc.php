d17a79103fac92277d6b19620c9cad8ca367ab9c
Gustaf Mossakowski <gustaf@koenige.org>
2025-10-09 19:38:36 +0200

    eventmenu: activate page eventmenu, check for menus for event and main event; add .below class for links below, remove link if site is identical;

--- zzbrick_page/eventmenu.inc.php
+++ zzbrick_page/eventmenu.inc.php
@@ -15,29 +15,41 @@
 
 function page_eventmenu() {
 	global $zz_page;
-	// @todo not active yet
-	return '';
-	
 	if (empty($zz_page['db'])) return '';
 	$params = explode('/', $zz_page['db']['parameter']);
 	if (count($params) < 2) return '';
 
+	// is it an event with eventmenus?
+	// check for main event having an event menu first
 	$sql = 'SELECT eventmenu_id
-			, menu, path, parameters
+			, menu, path, eventmenus.parameters
 			, IFNULL(event_year, YEAR(date_begin)) AS year
 		FROM eventmenus
-		LEFT JOIN events USING (event_id)
-		WHERE identifier = "%d/%s"
+		LEFT JOIN events
+			ON eventmenus.event_id = IFNULL(events.main_event_id, events.event_id)
+		WHERE events.identifier = "%d/%s"
 		ORDER BY eventmenus.sequence';
 	$sql = sprintf($sql, $params[0], wrap_db_escape($params[1]));
 	$data = wrap_db_fetch($sql, 'eventmenu_id');
 	if (!$data) return '';
 
 	$placeholder_keys = ['menu', 'path'];
-	foreach ($data as &$line) {
+
+	foreach ($data as $eventmenu_id => $line) {
+		// remove anchor for comparison
+		if ($pos = strpos($line['path'], '#'))
+			$line['path'] = substr($line['path'], 0, $pos);
+
+		// check if menu is active or below
+		if ($line['path'] === wrap_setting('request_uri'))	
+			$data[$eventmenu_id]['path'] = false;
+		elseif (str_starts_with(wrap_setting('request_uri'), $line['path']))
+			$data[$eventmenu_id]['below'] = true;
+		
+		// placeholders
 		foreach ($placeholder_keys as $key) {
 			if (strstr($line[$key], '%%% year %%%'))
-				$line[$key] = str_replace('%%% year %%%', $line['year'], $line[$key]);
+				$data[$eventmenu_id][$key] = str_replace('%%% year %%%', $line['year'], $line[$key]);
 		}
 	}
 	
