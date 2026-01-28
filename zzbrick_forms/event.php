f062bad6ec4e780e4df0f92e926427ed5b47e403
Gustaf Mossakowski <gustaf@koenige.org>
2026-01-28 14:19:25 +0100

    event form: add title, subtitle for form

--- zzbrick_forms/event.php
+++ zzbrick_forms/event.php
@@ -8,7 +8,7 @@
  * https://www.zugzwang.org/modules/events
  *
  * @author Gustaf Mossakowski <gustaf@koenige.org>
- * @copyright Copyright © 2023-2024 Gustaf Mossakowski
+ * @copyright Copyright © 2023-2024, 2026 Gustaf Mossakowski
  * @license http://opensource.org/licenses/lgpl-3.0.html LGPL-3.0
  */
 
@@ -17,6 +17,15 @@ if (empty($brick['data']['event_id'])) wrap_quit(404);
 
 $zz = zzform_include('events', [], 'forms');
 $zz['where']['event_id'] = $brick['data']['event_id'];
+$zz['title'] = 'Event';
 
 $zz['access'] = 'edit_only';
 $zz['page']['referer'] = '../';
+
+$zz['subtitle']['event_id']['sql'] = 'SELECT event_id, event
+	, CONCAT(events.date_begin, IFNULL(CONCAT("/", events.date_end), "")) AS duration
+	FROM events';
+$zz['subtitle']['event_id']['var'] = ['event', 'duration'];
+$zz['subtitle']['event_id']['format'][1] = 'wrap_date';
+$zz['subtitle']['event_id']['link'] = '../';
+$zz['subtitle']['event_id']['link_no_append'] = true;
