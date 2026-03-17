2abbb1e942ece759e6df145b3f541dd6248bcf80
Gustaf Mossakowski <gustaf@koenige.org>
2026-03-17 12:52:10 +0100

    project: use wrap_routes_page_ids() instead of old function name

--- zzbrick_request/project.inc.php
+++ zzbrick_request/project.inc.php
@@ -115,7 +115,7 @@ function mod_events_project($params) {
 	if (!$event['published'])
 		$page['extra']['class'] = 'unpublished';
 	if (!empty($event['menu_hierarchy']))
-		$page['extra']['menu_hierarchy'] = wrap_menu_hierarchy('events_projects', $event['menu_hierarchy'], 'category');
+		$page['extra']['menu_hierarchy'] = wrap_routes_page_ids('events_projects', $event['menu_hierarchy'], 'category');
 	$page['data'] = $event;
 
 	return $page;
