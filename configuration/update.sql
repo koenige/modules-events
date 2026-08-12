6a27df126ecdb02d0b1204ff58b0c7d2c8dfbfbf
Gustaf Mossakowski <gustaf@koenige.org>
2026-08-12 11:23:17 +0200

    definition: read from zzform_def[hide_in_list]

--- configuration/update.sql
+++ configuration/update.sql
@@ -98,3 +98,4 @@
 /* 2026-07-31-4 */	UPDATE categories SET parameters = REPLACE(parameters, '&events=1', '&events_contacts=1') WHERE parameters LIKE '%&events=1%';
 /* 2026-07-31-5 */	UPDATE categories SET parameters = REPLACE(parameters, '&hide_date_end=1', '&events_hide_date_end=1') WHERE parameters LIKE '%&hide_date_end=1%';
 /* 2026-08-12-1 */	INSERT INTO categories (`category`, `description`, `main_category_id`, `path`, `parameters`, `sequence`, `last_update`) VALUES ('Projects', NULL, NULL, 'projects', '&alias=projects', NULL, NOW());
+/* 2026-08-12-2 */	UPDATE categories SET parameters = REPLACE(parameters, '&hide_in_list=', '&zzform_def[hide_in_list]=') WHERE parameters LIKE '%&hide_in_list=%' AND main_category_id = /*_ID event _*/;
