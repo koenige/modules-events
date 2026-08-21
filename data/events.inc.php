763b5513675e904c58981a45cc3a9b0bef6b5b64
Gustaf Mossakowski <gustaf@koenige.org>
2026-08-21 10:07:27 +0200

    events: use categories.published, not parameter `hide_category` to show only published categories

--- data/events.inc.php
+++ data/events.inc.php
@@ -163,7 +163,7 @@ function mf_events_categories($events, $ids, $langs) {
 			ON main_categories.category_id = categories.main_category_id
 			AND categories.main_category_id != events_categories.type_category_id
 		WHERE event_id IN (%s)
-		AND (ISNULL(categories.parameters) OR categories.parameters NOT LIKE "%%&hide_category=1%%")
+		AND categories.published = "yes"
 		ORDER by categories.sequence, categories.category';
 	$sql = sprintf($sql, implode(',', $ids));
 	$data = wrap_db_fetch($sql, 'event_category_id');
